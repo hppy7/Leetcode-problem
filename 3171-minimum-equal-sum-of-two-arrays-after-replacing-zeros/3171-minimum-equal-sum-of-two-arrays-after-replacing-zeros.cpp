1349c4799c4b052260a3aa4a002a588b25bea8c2
SUNDHIR SINGH <happysohil1234@gmail.com>
2025-05-10 20:06:43 +0530

    Time: 145 ms (13.56%), Space: 135.3 MB (93.56%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -5,25 +5,32 @@ public:
 
  int zero1=0,zero2=0;
 long long sum1=0,sum2=0;
- for(int i=0;i<nums1.size();i++)
- {
-    if(nums1[i]==0 )
-    {
-    zero1++;
-    }
- }
+//  for(int i=0;i<nums1.size();i++)
+//  {
+//     if(nums1[i]==0 )
+//     {
+//     zero1++;
+//     }
+//  }
 
-for(int i=0;i<nums2.size();i++)
- {
-    if(nums2[i]==0 )
-    {
-    zero2++;
-    }
- }
+// for(int i=0;i<nums2.size();i++)
+//  {
+//     if(nums2[i]==0 )
+//     {
+//     zero2++;
+//     }
+//  }
 for(int i=0;i<nums1.size();i++)
+{
  sum1 += nums1[i] == 0 ? 1: nums1[i];
+ zero1 +=nums1[i]==0? 1: 0;
+  
+}
  for(int i=0;i<nums2.size();i++)
+ {
   sum2 +=nums2[i]==0?1:nums2[i];
+  zero2 +=nums2[i]==0? 1: 0;
+ }
  //int zero1=0,zero2=0;
 
 //  for(int i=0;nums1.size();i++)
