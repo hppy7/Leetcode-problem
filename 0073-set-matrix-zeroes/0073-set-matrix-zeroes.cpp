bc35ca1bfb1557a7a44801fa14a58d1883d67fcf
SUNDHIR SINGH <happysohil1234@gmail.com>
2025-05-21 21:40:28 +0530

    Time: 3 ms (11.48%), Space: 19.2 MB (6.61%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,67 +1,80 @@
-// class Solution {
-// public:
-//     void setZeroes(vector<vector<int>>& matrix) {
-//     int n = matrix.size();        // number of rows
-//         int m = matrix[0].size(); 
-//      for(int i=0;i<n;i++)
-//      {
-//           for(int j=0;j<m;j++)
-//           {
-
-//             if(matrix[i][j]==0)
-//             {
-//              for(int k=0;k<n;k++)   
-//              {
-//                 matrix[i][k]=0;
-
-//              }
-//               for(int k=0;k<m;k++)   
-//              {
-//                 matrix[k][j]=0;
-                
-//              }
-//             }
-//           }
-//      }
-
-//     }
-// };
-
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();        // number of rows
-        int m = matrix[0].size();     // number of columns
+    int n = matrix.size();        // number of rows
+        int m = matrix[0].size();
 
-        vector<bool> row(n, false);
-        vector<bool> col(m, false);
+          vector<vector<int>> mat2(n,vector<int>(m));
+      for(int i=0;i<n;i++)
+     {
+          for(int j=0;j<m;j++)
+          {
+            mat2[i][j]=matrix[i][j];
+          }
+          
+          
+          }
 
-        // First pass: mark rows and columns
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
-                }
+
+     for(int i=0;i<n;i++)
+     {
+          for(int j=0;j<m;j++)
+          {
+
+            if(matrix[i][j]==0)
+            {
+             for(int k=0;k<m;k++)   
+             {
+                mat2[i][k]=0;
+
+             }
+              for(int k=0;k<n;k++)   
+             {
+                mat2[k][j]=0;
+                
+             }
             }
-        }
-
-        // Second pass: set rows to 0
-        for (int i = 0; i < n; i++) {
-            if (row[i]) {
-                for (int j = 0; j < m; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Third pass: set columns to 0
-        for (int j = 0; j < m; j++) {
-            if (col[j]) {
-                for (int i = 0; i < n; i++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+          }
+     }
+   matrix = mat2;
     }
 };
+
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         int n = matrix.size();        // number of rows
+//         int m = matrix[0].size();     // number of columns
+
+//         vector<bool> row(n, false);
+//         vector<bool> col(m, false);
+
+//         // First pass: mark rows and columns
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     row[i] = true;
+//                     col[j] = true;
+//                 }
+//             }
+//         }
+
+//         // Second pass: set rows to 0
+//         for (int i = 0; i < n; i++) {
+//             if (row[i]) {
+//                 for (int j = 0; j < m; j++) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+
+//         // Third pass: set columns to 0
+//         for (int j = 0; j < m; j++) {
+//             if (col[j]) {
+//                 for (int i = 0; i < n; i++) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+//     }
+// };
