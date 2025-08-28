82fb844231160e4ba15d756f2265944fa823c208
SUNDHIR SINGH <happysohil1234@gmail.com>
2025-08-28 23:32:26 +0530

    Time: 54 ms (5.18%), Space: 8.5 MB (32.69%) - LeetHub

---
+++ 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxContainers(int n, int w, int maxWeight) {
+        
+     int c=0;
+     int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {    sum+=w;
+                if(sum>maxWeight)
+                {
+                  return c;
+                }
+                c++;
+            }
+        }
+        return c;
+    }
+};
