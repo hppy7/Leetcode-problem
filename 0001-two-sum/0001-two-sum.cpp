c73e7648ca40746d7e95aa2c692745a15c8e8fa2
SUNDHIR SINGH <happysohil1234@gmail.com>
2025-11-17 16:14:01 +0530

    Time: 13 ms (39.84%), Space: 18.6 MB (5.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,51 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-map<int,int>mp;
+//map<int,int>mp;
+map<int, vector<int>> mp;
+ int n=nums.size();
+// for(int i=0;i<n;i++)
+// {
+//     int t=target-nums[i];
+//     if(mp.find(t)!=mp.end())
+//     {
+//        return {i,mp[t]};
 
-int n=nums.size();
-for(int i=0;i<n;i++)
-{
-    int t=target-nums[i];
-    if(mp.find(t)!=mp.end())
-    {
-       return {i,mp[t]};
-
-    }
+//     }
     
 
-    mp[nums[i]]=i;
+//     mp[nums[i]]=i;
+// }
+
+
+//     return {};   
+
+for(int i=0;i<n;i++)
+{
+    mp[nums[i]].push_back(i);
 }
-
-
-    return {};           
+sort(nums.begin(),nums.end());
+int i=0,j=n-1;
+while(i<j)
+{
+    int t=nums[i]+nums[j];
+    
+    if(t>target)
+    {
+        j--;
+    }else if(t<target)
+    {
+        i++;
+    }else
+    {
+        if(nums[i]==nums[j])
+        {
+            return {mp[nums[i]][0], mp[nums[i]][1]};
+        }
+      return {mp[nums[i]][0], mp[nums[j]][0]};
+    }
+    
+}
+return {};
     }
 };
