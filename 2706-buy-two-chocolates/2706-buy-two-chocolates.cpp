bc1bcd17d85cfdef51e4101e553ca3e133d93713
SUNDHIR SINGH <happysohil1234@gmail.com>
2025-12-20 20:01:04 +0530

    Time: 0 ms (100%), Space: 52.1 MB (66.62%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+
+        int n=prices.size();
+
+
+
+
+        sort(prices.begin(),prices.end());
+
+        int ans=money-(prices[0]+prices[1]);
+
+
+        if(ans<0)
+        return money;
+
+         return ans;
+        
+    }
+};
