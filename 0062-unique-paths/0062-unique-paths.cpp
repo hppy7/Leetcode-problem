e9783dae68d062b0ae75fbbe84f73069848bf91f
SUNDHIR SINGH <happysohil1234@gmail.com>
2026-02-04 18:34:30 +0530

    Time: 0 ms (100%), Space: 7.8 MB (97.2%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-   int t[101][101];
-  int sol(int i,int j,int m ,int n)
-  {
+int t[101][101];
+int sol(int i,int j)
+{
 
+if(i==0 && j==0)
+return 1;
 
-    if(i>m || i<0|| j>n || j<0)
-    return 0;
-
-    if(i==m-1 && j==n-1)
-    {
-        return 1;
-    }
-
-
-  if(t[i][j]!=-1)
-  return t[i][j];
-    int r= sol(i,j+1,m,n);
-    int l= sol(i+1,j,m,n);
-    return t[i][j]= r+l;
-  }
+if(i<0 || j<0)
+return 0;
+   if(t[i][j]!=-1)
+   return t[i][j];
+    return t[i][j]=sol(i-1,j)+sol(i,j-1);
+}
     int uniquePaths(int m, int n) {
 
-       memset(t,-1,sizeof(t));
-        return sol(0,0,m,n);
+      memset(t,-1,sizeof(t));
+        return sol(m-1,n-1);
         
     }
 };
